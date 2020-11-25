adf316cddc05309672ede211b7c6fee7dbd9d262
Nijat Mursali <nijatmursali@gmail.com>
2020-11-25 02:34:03 +0100

    navbar finished

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/data/data.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text('Food Delivery'),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: () {},
+              child: Text(
+                'Card (${currentUser.cart.length})',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ))
+        ],
+      ),
+    );
+  }
+}
