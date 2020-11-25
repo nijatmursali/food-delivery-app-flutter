ae8cf7035a1bea18c3b83f81c6b876df84a08da9
Nijat Mursali <nijatmursali@gmail.com>
2020-11-25 21:21:09 +0100

    search bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,22 +10,50 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.account_circle),
-          iconSize: 30.0,
-          onPressed: () {},
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.account_circle),
+            iconSize: 30.0,
+            onPressed: () {},
+          ),
+          title: Text('Food Delivery'),
+          actions: <Widget>[
+            FlatButton(
+                onPressed: () {},
+                child: Text(
+                  'Card (${currentUser.cart.length})',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ))
+          ],
         ),
-        title: Text('Food Delivery'),
-        actions: <Widget>[
-          FlatButton(
-              onPressed: () {},
-              child: Text(
-                'Card (${currentUser.cart.length})',
-                style: TextStyle(color: Colors.white, fontSize: 20.0),
-              ))
-        ],
-      ),
-    );
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(width: 0.8),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide(
+                        width: 0.8, color: Theme.of(context).primaryColor),
+                  ),
+                  hintText: 'Search for food or restaurant',
+                  prefixIcon: Icon(Icons.search, size: 30.0),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
